05f58510a6c26ccc84075eb25ead099a826f1503
Peter Symoens <peter.symoens@eventures.io>
2015-06-19 09:49:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+##CSS animated pre-loader Angular directive
+
+###Usage:
+
+* Add the interceptor to your angular module
+* Add an ENV constant with REST_URL (API endpoint) and REQUEST_TIMEOUT properties
+* Add a loader element to your view, specifying the type of CSS animation as an attribute
+* Uncomment the css import statement for the animation you want to use in loader.scss
+
+
+
+CSS animations are from http://tobiasahlin.com/spinkit/
---
+++ src/js/loader.js
@@ -0,0 +1,94 @@
+'use strict';
+angular.module('evtrs.loader', []).
+    factory('HttpRequestInterceptor', function ($rootScope, $q, ENV, $log) {
+        var pendingRequests = 0;
+
+        var checkUrl = function (config) {
+            if (!ENV.REST_URL) {
+                ENV.REST_URL = 'api';
+            }
+            return config.url.indexOf(ENV.REST_URL) != -1 ? true : false;
+        }
+
+        return {
+            request: function (config) {
+                config.timeout = ENV.REQUEST_TIMEOUT ? ENV.REQUEST_TIMEOUT : 20000;
+                if (checkUrl(config)) {
+                    pendingRequests++;
+                    $rootScope.$broadcast('HTTP_START');
+                }
+                return config || $q.when(config)
+            },
+            response: function (response) {
+                if (checkUrl(response.config)) {
+                    if ((--pendingRequests) === 0) {
+                        $rootScope.$broadcast('HTTP_STOP');
+                    }
+                }
+                return response || $q.when(response);
+            },
+            responseError: function (response) {
+                if (checkUrl(response.config)) {
+                    if (!(--pendingRequests)) {
+                        $rootScope.$broadcast('HTTP_STOP');
+                    }
+                }
+                if (!response.statusText) {
+                    response.statusText = 'No Response';
+                }
+                $log.error('http error ' + response.status ? response.status : 'UNKNOWN',
+                    response.statusText ? response.statusText : 'UNKNOWN',
+                    response.config.url ? response.config.url : 'UNKNOWN');
+
+                $rootScope.$broadcast('STATUS_ERROR', {type: 'HTTP_ERROR',
+                    code: response.status ? response.status : 'UNKNOWN',
+                    message: response.statusText ? response.statusText : 'UNKNOWN',
+                    cause: response.config.url ? response.config.url : 'UNKNOWN'});
+                return $q.reject(response);
+            }
+        };
+    })
+    .directive('loader', function () {
+        return {
+            restrict: 'E',
+            scope: {
+
+            },
+            link: function (scope, element, attrs) {
+                var spinner = $document.createElement('div');
+                switch (attrs.animation) {
+                    case  box:
+                        spinner.setAttribute('class', 'loader-box');
+                        break;
+                    case bounce:
+                        spinner.setAttribute('class', 'loader-bounce');
+                        var bounce1 = $document.createElement('div');
+                        bounce1.setAttribute('class', 'loader-bounce1');
+                        spinner.append(bounce1);
+                        var bounce2 = $document.createElement('div');
+                        bounce2.setAttribute('class', 'loader-bounce2');
+                        spinner.append(bounce2);
+                        break;
+                    default:
+                        spinner.setAttribute('class', 'loader-box');
+                        break;
+                }
+                element.append(spinner);
+
+            },
+            controller: function ($scope, $element) {
+
+                $scope.$on('HTTP_START', function () {
+                    $element.css('display', 'block');
+                });
+
+                $scope.$on('HTTP_STOP', function () {
+                    $element.css('display', 'none');
+                });
+
+            }
+
+        }
+
+
+    })
